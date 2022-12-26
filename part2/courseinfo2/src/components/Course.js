5802c4ad409cf9af5db55465da7115a8bd2c81f9
Patrick Salmi <patrick.salmi@helsinki.fi>
2022-12-26 23:33:22 +0200

    separated components in Course.js

--- part2/courseinfo2/src/components/Course.js
+++ part2/courseinfo2/src/components/Course.js
@@ -1,29 +1,33 @@
-const Course = ({ course }) => {
-    const Header = () => <h2>{course.name}</h2>
-    const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+const Header = ({ name }) => (
+  <h2>{name}</h2>
+)
 
-    const Content = () => (
-        <>
-          {course.parts.map(part =>
-            <Part key={part.id} part={part} />
-          )}
-        </>
-      )
+const Part = ({ part }) => (
+  <p>{part.name} {part.exercises}</p>
+)
 
-    const Total = () => {
-        const sum = course.parts.reduce((s, p) => s = s + p.exercises, 0)
-        return (
-            <p><b>total of {sum} exercises</b></p>
-        )
-    }
-      
-    return (
-      <>
-        <Header/>
-        <Content/>
-        <Total/>
-      </>
-    )
-  }
+const Content = ({ parts }) => (
+  <>
+    {parts.map(part =>
+      <Part key={part.id} part={part} />
+    )}
+  </>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((s, p) => s = s + p.exercises, 0)
+  return (
+    <p><b>total of {sum} exercises</b></p>
+  )
+}
+
+const Course = ({ course }) => (
+  <>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </>
+)
+
 
 export default Course
