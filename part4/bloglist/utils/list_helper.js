762be75aed2e18da571fd27b3ec71122b550f50b
PatrickSalmi <patrick.salmi@helsinki.fi>
2023-09-21 01:25:21 +0300

    added 4.5 - bloglist

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,23 @@ const dummy = (blogs) => (1)
 
 const totalLikes = (blogs) => (blogs.reduce((sum, blog) => sum + blog.likes, 0))
 
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce(
+    (prev, current) =>
+      prev.likes > current.likes ? prev : current
+  )
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
